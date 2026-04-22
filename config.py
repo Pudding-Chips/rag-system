e210afe87c2403f4d3d2657ea81faf16de97fc7e
Pudding-Chips <83540688+Pudding-Chips@users.noreply.github.com>
2026-04-22 22:26:07 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-# =========================
-# config.py（统一配置）
-# =========================
-
 DB_HOST = "localhost"
 DB_PORT = 8000
 
@@ -9,8 +5,6 @@ DEFAULT_MODEL = "text2vec"
 
 EMBEDDING_MODELS = {
     "text2vec": "shibing624/text2vec-base-chinese",
-    # 以后可以加：
-    # "bge": "BAAI/bge-base-zh",
 }
 
 COLLECTION_TEMPLATE = "{biz}_{model}_v1"
